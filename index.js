c4a0e4503662c0179713da8ec522242dcb6c7292
Maryinfun <motmaryion@gmail.com>
2023-05-01 00:03:09 +0300

    feat: print with mouse, backspace, delete

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
+// backSpace = optnlBtn[0];
+// tab = optnlBtn[1];
+// delete = optnlBtn[2];
+// capsLock = optnlBtn[3];
+// enter = optnlBtn[4];
+// shiftLeft = optnlBtn[5];
+// cursorTop = optnlBtn[6];
+// shiftRight = optnlBtn[7];
+// ctrlLeft = optnlBtn[8];
+// cursorLeft = optnlBtn[13];
+// cursorBottom = optnlBtn[14];
+// cursorRight = optnlBtn[15];
+
 class BtnMarks {
   constructor() {
-    this.engMain = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', "'", '\\', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '.', ',', '/'];
+    this.engMain = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', "'", '\\', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '.', ',', '/', ' '];
     this.engAbove = ['~', '!', '@', '#', '$', '%', ':', '?', '', '(', ')', '_', '+', '/'];
-    this.ruMain = ['Ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '\\', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', '\\', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', '/'];
+    this.ruMain = ['Ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '\\', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', '\\', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', '/', ' '];
     this.ruAbove = ['', '!', '"', '№', ';', '%', '^', '&', '', '(', ')', '_', '+', '/'];
     this.cntrlBtn = ['Backspace', 'Tab', 'DEL', 'Caps Lock', 'ENTER', 'Shift', 'Ʌ', 'Shift', 'Ctrl', 'Win', 'Alt', '', 'Alt', 'Ctrl', '<', 'V', '>'];
-    this.btnCode = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'IntlBackslash', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Period', 'Comma', 'Slash', 'Space'];
+    this.engMainOnlyCode = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'IntlBackslash', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Period', 'Comma', 'Slash', 'Space'];
+    this.ruMainOnlyCode = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'IntlBackslash', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'Space'];
+    this.allValueCode = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace', 'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'Delete', 'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter', 'ShiftLeft', 'IntlBackslash', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Period', 'Comma', 'Slash', 'ArrowUp', 'ShiftRight', 'ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ControlRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight'];
+    this.aboveOnlyCode = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backslash'];
   }
 }
 
 const language = localStorage.getItem('language') || 'en';
 const body = document.querySelector('body');
-// const wrapper = document.createElement("div");
 const sectionText = document.createElement('section');
 const screen = document.createElement('textarea');
 const sectionKeyboard = document.createElement('section');
@@ -21,7 +36,7 @@ sectionText.className = 'top';
 body.appendChild(sectionText);
 screen.className = 'input__text';
 if (language === 'en') {
-  screen.placeholder = 'Attention please, the keyboard is created in Windows.\nPress SHIFT + CTRL to change language.';
+  screen.placeholder = 'Attention please, the keyboard was created in Windows.\nPress SHIFT + CTRL to change language.';
 } else {
   screen.placeholder = 'Внимание, клавиатура создана в Windows.\nДля смены языка нажмите SHIFT + CTRL.';
 }
@@ -72,15 +87,16 @@ for (let i = 0; i < 65; i += 1) {
 }
 
 // BUTTON MARKS
-const item = new BtnMarks();
-const btnSet = Array.from(document.querySelectorAll('.keyboard__btn'));
-const mainBtn = btnSet.filter((key, i) => i < 13 || (i > 14 && i < 28)
-|| (i > 29 && i < 41) || (i > 42 && i < 54));
-const upperBtn = btnSet.filter((key, i) => i < 13 || i === 27);
-const optnlBtn = btnSet.filter((key, i) => i === 13 || i === 14
+function addSign() {
+  const item = new BtnMarks();
+
+  const btnSet = Array.from(document.querySelectorAll('.keyboard__btn'));
+  const mainBtn = btnSet.filter((key, i) => i < 13 || (i > 14 && i < 28)
+|| (i > 29 && i < 41) || (i > 42 && i < 54) || i === 59);
+  const upperBtn = btnSet.filter((key, i) => i < 13 || i === 27);
+  const optnlBtn = btnSet.filter((key, i) => i === 13 || i === 14
 || i === 28 || i === 29 || i === 41 || i === 42 || i >= 54);
 
-function addSign() {
   mainBtn.forEach((key, i) => {
     const button = key;
     if (language === 'en') button.innerText = item.engMain[i];
@@ -100,7 +116,103 @@ function addSign() {
     button.innerText = item.cntrlBtn[i];
   });
 }
-
 addSign();
 
-// console.log(String.fromCharCode(8593, 8594, 8595, 8592), extraButton, item.ruMain.length);
+// USE MOUSE INPUT
+function print(textarea, text) {
+  const message = textarea;
+  const endWith = message.value.substring(0, message.selectionStart)
+  + text + message.value.substring(message.selectionEnd);
+  message.value = endWith;
+  message.focus();
+  message.selectionEnd = message.selectionStart === message.selectionEnd
+    ? message.selectionEnd + text.length : message.selectionEnd;
+}
+
+function deletePrint(textarea) {
+  const message = textarea;
+  const endWith = message.value.substring(0, message.selectionStart)
+    + message.value.substring(message.selectionEnd + 1);
+  message.value = endWith;
+  message.focus();
+  message.selectionEnd = message.selectionStart === message.selectionEnd
+    ? message.selectionStart : message.selectionEnd;
+}
+
+function backspacePrint(textarea) {
+  const message = textarea;
+  const endWith = message.value.substring(0, message.selectionStart - 1)
+    + message.value.substring(message.selectionEnd);
+  message.value = endWith;
+  message.focus();
+  message.selectionEnd = message.selectionStart !== 0
+    ? message.selectionEnd - 1 : message.selectionStart;
+}
+
+const item = new BtnMarks();
+const btnSet = Array.from(document.querySelectorAll('.keyboard__btn'));
+const mainBtn = btnSet.filter((key, i) => i < 13 || (i > 14 && i < 28)
+|| (i > 29 && i < 41) || (i > 42 && i < 54) || i === 59);
+const optnlBtn = btnSet.filter((key, i) => i === 13 || i === 14
+|| i === 28 || i === 29 || i === 41 || i === 42 || i >= 54);
+
+mainBtn.forEach((key, i) => key.addEventListener('click', () => {
+  if (language === 'en') {
+    if (optnlBtn[3].classList.contains('btn-active') && (optnlBtn[5].classList.contains('btn-active') || optnlBtn[7].classList.contains('btn-active'))) {
+      print(screen, item.engMain[i]);
+    } else if (item.aboveOnlyCode.indexOf(key.code) !== -1 && ((optnlBtn[5].classList.contains('btn-active') || optnlBtn[7].classList.contains('btn-active')))) {
+      print(screen, item.engMain[i]);
+    } else if ((optnlBtn[3].classList.contains('btn-active') || optnlBtn[5].classList.contains('btn-active') || optnlBtn[7].classList.contains('btn-active'))) {
+      print(screen, item.engMain[i].toLocaleUpperCase());
+    } else (print(screen, item.engMain[i]));
+  } else if (language === 'ru') {
+    if (optnlBtn[3].classList.contains('btn-active') && (optnlBtn[5].classList.contains('btn-active') || optnlBtn[7].classList.contains('btn-active'))) {
+      print(screen, item.ruMain[i]);
+    } else if (item.extraCode.indexOf(key.code) !== -1 && ((optnlBtn[5].classList.contains('btn-active') || optnlBtn[7].classList.contains('btn-active')))) {
+      print(screen, item.ruMain[i]);
+    } else if ((optnlBtn[3].classList.contains('btn-active') || optnlBtn[5].classList.contains('btn-active') || optnlBtn[7].classList.contains('btn-active'))) {
+      print(screen, item.ruMain[i].toLocaleUpperCase());
+    } else (print(screen, item.ruMain[i]));
+  }
+}));
+
+optnlBtn.forEach((key) => key.addEventListener('click', (el) => {
+  if (el.target === optnlBtn[2]) {
+    deletePrint(screen);
+  }
+  if (el.target === optnlBtn[0]) {
+    backspacePrint(screen);
+  }
+  if (el.target === optnlBtn[1]) {
+    print(screen, '    ');
+  }
+  if (el.target === optnlBtn[13]) {
+    print(screen, String.fromCharCode(8592));
+  }
+  if (el.target === optnlBtn[6]) {
+    print(screen, String.fromCharCode(8593));
+  }
+  if (el.target === optnlBtn[15]) {
+    print(screen, String.fromCharCode(8594));
+  }
+  if (el.target === optnlBtn[14]) {
+    print(screen, String.fromCharCode(8595));
+  }
+  if (el.target === optnlBtn[4]) {
+    print(screen, '\n');
+  }
+  if (el.target === optnlBtn[3] || el.target === optnlBtn[5] || el.target === optnlBtn[7]) {
+    el.target.classList.toggle('btn-active');
+  }
+  if (el.target === optnlBtn[8] && optnlBtn[5].classList.contains('btn-active')) {
+    if (localStorage.getItem('language') === 'ru') {
+      localStorage.setItem('language', 'en');
+      addSign();
+      optnlBtn[5].classList.remove('button-active');
+    } else {
+      localStorage.setItem('language', 'ru');
+      addSign();
+      optnlBtn[5].classList.remove('button-active');
+    }
+  }
+}));
